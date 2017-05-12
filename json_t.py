047df88d5563efaf3893d1ff492dd3e3a9e4118c
Jaeger <chenxj_1@126.com>
2017-05-12 15:50:15 +0800

    json测试样例

---
+++ json_t.py
@@ -0,0 +1,18 @@
+import json
+class Student(object):
+	def __init__(self, name, age, score):
+		self.name = name
+		self.age = age
+		self.score = score
+
+
+def studentToDict(stu):
+	return {
+		'name' : stu.name
+		'age'  : stu.age
+		'score' : stu.score
+	}
+
+
+print (json.dumps(s, default=studentToDict))
+print (json.dumps(s, default=lambda obj:obj.__dict__))
